b90a8b9cb26bf0ee36a370c33d714c9594230567
Rawit Panjaroen <check.rawit@gmail.com>
2020-08-17 03:56:05 +0700

    refactor: remove unused imported variable

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Redirect, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import PhotoResultPage from "./pages/PhotoResult";
 import DashboardPage from "./pages/Dashboard";
